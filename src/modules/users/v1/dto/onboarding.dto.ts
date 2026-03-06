23996a9c8000fa2bc1b4b293ee512ba4b3bc8d4d
YB009 <owohdaniel09@gmail.com>
2026-03-06 14:06:16 +0100

    NEW COMMIT

--- src/modules/users/v1/dto/onboarding.dto.ts
+++ src/modules/users/v1/dto/onboarding.dto.ts
@@ -12,7 +12,7 @@ import { ApiProperty } from '@nestjs/swagger';
 export enum ShopperStyle {
   MINIMALIST = 'minimalist',
   STREET_WEAR = 'street wear',
-  Y2K = 'y2k',
+  Y2K = 'y2kr',
   CASUAL = 'casual',
   FORMAL = 'formal',
   VINTAGE = 'vintage',
